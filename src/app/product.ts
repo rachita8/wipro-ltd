8d66af04a49a9d2a0573ff59b43fd529257fa41b
rachita8 <70127514+rachita8@users.noreply.github.com>
2022-06-30 20:29:25 +0530

    Add files via upload

---
+++ src/app/product.ts
@@ -0,0 +1,11 @@
+//ng generate class product
+
+export class Product {
+
+    id: string | any;
+    name: string | any;
+    brand: string | any;
+    madein: string | any;
+    price:number | any;
+    active: boolean = false;
+}
